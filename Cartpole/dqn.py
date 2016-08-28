de87367c23f01cd4e8008a858ea05ffa6eb71f7d
Gautham Vasan <gauthamv.529@gmail.com>
2016-08-27 20:22:53 -0600

    Working DQN and only Neural net representation

--- Cartpole/dqn.py
+++ Cartpole/dqn.py
@@ -17,6 +17,8 @@ import math
 env = gym.make('CartPole-v0')
 
 # Initialize memory parameters
+mem_buffer = []
+replay_capacity = 500
 input_shape = 4
 batch_size = 100
 gamma = 0.99
@@ -24,8 +26,6 @@ epsilon_q = 1.0
 num_actions = 2
 numEpisodes = 500
 numRuns = 10
-num_channels = 5
-batch_mem = []
 
 # Define the model
 model = Sequential()
@@ -42,6 +42,55 @@ model.add(Dense(num_actions, activation='linear')) # This special "softmax" acti
 #model.compile(loss='mean_squared_error', optimizer=SGD(lr=math.pow(10,-4), momentum=0.9, nesterov=True))
 model.compile(loss='mse', optimizer='adam')
 
+class replay_memory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.sample_count = 0
+        self.index = 0
+        self.full = False
+        self.state1_memory = np.zeros((capacity, input_shape))
+        self.action_memory = np.zeros(capacity, dtype=np.uint8)
+        self.reward_memory = np.zeros(capacity, dtype=np.uint8)
+        self.done_memory = np.zeros(capacity, dtype=np.uint8)
+        self.greedy_memory = np.zeros(capacity, dtype=np.uint8)
+        self.state2_memory = np.zeros((capacity, input_shape))
+
+    def add_entry(self, state1, action, reward, state2, done, greedy):
+        self.state1_memory[self.index, :] = state1
+        self.state2_memory[self.index, :] = state2
+        self.action_memory[self.index] = action
+        self.reward_memory[self.index] = reward
+        self.done_memory[self.index] = done
+        self.greedy_memory[self.index] = greedy
+        self.index += 1
+        if(self.index>=self.capacity):
+            self.full = True
+            self.index = 0
+        if not self.full:
+            self.size += 1
+
+    def sample_batch_random(self, size):
+        batch = np.random.choice(np.arange(0,self.size), size=size)
+        states1 = self.state1_memory[batch]
+        states2 = self.state2_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        done = self.done_memory[batch]
+        greedy = self.greedy_memory[batch]
+        return (states1, actions, rewards, states2, done, greedy)
+
+    def sample_batch(self, batch_size):
+        start = self.size - batch_size
+        end = self.size
+        states1 = self.state1_memory[start:end]
+        states2 = self.state2_memory[start:end]
+        actions = self.action_memory[start:end]
+        rewards = self.reward_memory[start:end]
+        done = self.done_memory[start:end]
+        greedy = self.greedy_memory[start:end]
+        return (states1, actions, rewards, states2, done, greedy)
+
 
 def epsilon_greedy_policy(state):
     rand = random.random()
@@ -56,20 +105,16 @@ def epsilon_greedy_policy(state):
         return scores.argmax(), greedy
 
 def Q_update():
-    states1, states2 = [np.zeros((batch_size,input_shape)) for _ in xrange(2)]
-    actions, rewards,  done =  [np.zeros((batch_size)) for _ in xrange(3)]
+    if mem.size > batch_size:
+        size = batch_size
+    else:
+        size = np.random.randint(mem.size) + 1
+    states1, actions, rewards, states2, done, greedy = mem.sample_batch_random(size)
     #targets = np.zeros((rewards.size,num_actions))
-    for i in range(batch_size):
-        states1[i] = batch_mem[i][0]
-        actions[i] = batch_mem[i][1]
-        rewards[i] = batch_mem[i][2]
-        states2[i] = batch_mem[i][3]
-        done[i] = batch_mem[i][4]
-
     old_Q = model.predict(states1)
     new_Q = model.predict(states2)
     targets = np.copy(old_Q)
-    for i in range(batch_size):
+    for i in range(size):
         if done[i]:
             targets[i,actions[i]] = rewards[i]
         else:
@@ -77,13 +122,10 @@ def Q_update():
         #print(actions[i], targets[i,actions[i]] - old_Q[i,actions[i]])
     model.train_on_batch(states1,targets)
 
-def buffer_func(current_state, action, reward, next_state, done):
-    if len(batch_mem) == batch_size:
-        batch_mem.remove(batch_mem[0])
-    batch_mem.append([current_state, action, reward, next_state, done])
+
+mem = replay_memory(replay_capacity)
 
 if __name__ == '__main__':
-    sample_count = 0
     for i_episode in range(numEpisodes):
         current_state = env.reset()
         t = 0
@@ -94,12 +136,11 @@ if __name__ == '__main__':
             #print(current_state,type(current_state),env.action_space.sample())
             next_state, reward, done, info = env.step(action)
             returns += reward
-            buffer_func(current_state, action, reward, next_state, done)
-            if sample_count >= batch_size:
-                Q_update()
+            mem.add_entry(current_state, action, reward, next_state, done, greedy)
+            Q_update()
             current_state = next_state
             t += 1
-            sample_count += 1
+            mem.sample_count += 1
             if done or returns>200:
                 print("Episode " + str(i_episode) + " finished after {} timesteps with {} reward and {} epsilon".format(t+1,returns,epsilon_q))
                 break
@@ -108,7 +149,7 @@ if __name__ == '__main__':
 
     # serialize model to JSON
     model_json = model.to_json()
-    with open("neural_net.json", "w") as json_file:
+    with open("dqn.json", "w") as json_file:
         json_file.write(model_json)
     # serialize weights to HDF5
     model.save_weights("model.h5")
